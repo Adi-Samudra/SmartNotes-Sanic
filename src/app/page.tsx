98f2549845b41edd2fc10ef01a7996bca23557f0
Adi-Samudra <adityasamudra369@gmail.com>
2025-01-31 22:12:47 +0530

    Responsiveness 375 working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,31 +106,51 @@ export default function Home() {
 
 
   return (
-    <div className="h-fit mx-auto px-4 xs:px-0 sm:px-6 lg:px-8 bg-black sm:bg-black lg:bg-black border-t-2 border-[#017373]">
-      <div className="flex w-full h-fit justify-between">
-        <div className="lg:py-32 lg:px-32 md:px-14 md:py-20 sm:px-10 sm:py-14 xs:px-2 xs:py-6 w-full lg:w-3/5 h-fit text-white ">
-          <h1 className="text-6xl xs:text-2xl font-text font-bold"><span className="bg-gradient-to-tr from-[#017373] to-[#AAD9D1] text-transparent bg-clip-text">Intelligent</span> cloud-based
-            <span className="bg-gradient-to-tr from-[#017373] to-[#AAD9D1] text-transparent bg-clip-text "> note-taking </span> and collaboration tool</h1>
-          <p className="font-text py-1">Experience the power of smart note-taking and transform the way you work today.</p>
-          <div className="flex w-fit h-fit gap-1 bg-white my-14 xs:my-4 rounded-md">
-            <input className=" sm:w-[221px] xs:w-[60px] text-black m-2 px-2 text-md font-text ">
-            </input>
-            <div className="flex-shrink-1">
-              <button className="justify-self-end flex items-center gap-2 m-1 py-1 px-4 bg-gradient-to-tr text-black font-semibold font-text from-[#017373] to-[#AAD9D1] rounded-lg">
-                Sign up
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="my-2">
-                  <rect x="0.5" y="0.5" width="23" height="23" rx="4.5" stroke="#333333" />
-                  <path d="M6.60583 6.43717V9.38062H12.2096V6.43717H6.60583Z" fill="#333333" />
-                  <path d="M15.1535 6.43709L12.2099 9.38069L12.2088 9.38175L11.8276 9.38175L11.8276 9.76297L5.47052 16.1201L7.55132 18.2009L14.2907 11.4615L14.2923 17.0466L17.2353 17.046V9.38122L17.2337 8.51842L17.2343 8.51789L17.2343 6.43869L17.2327 6.43709L15.1535 6.43709Z" fill="#333333" />
-                </svg>
-              </button>
-            </div>
+    <div className="h-fit px-4 xs:px-0 xs:pl-16 sm:px-6 lg:px-8 bg-black sm:bg-black lg:bg-black border-t-2 border-[#017373] max-w-full">
+      <div className="flex flex-col xl:flex-row w-full items-center justify-between bg-black">
+      <div className="w-full xl:w-3/5 text-white px-4 sm:px-10 md:px-14 lg:px-32 py-6 sm:py-14 md:py-20 lg:py-32">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-text font-bold leading-tight">
+          <span className="bg-gradient-to-tr from-[#017373] to-[#AAD9D1] text-transparent bg-clip-text">
+            Intelligent
+          </span>{" "}
+          cloud-based
+          <span className="bg-gradient-to-tr from-[#017373] to-[#AAD9D1] text-transparent bg-clip-text">
+            {" "}
+            note-taking{" "}
+          </span>{" "}
+          and collaboration tool
+        </h1>
+        <p className="font-text py-4 text-sm sm:text-base">
+          Experience the power of smart note-taking and transform the way you work today.
+        </p>
+        <div className="flex w-fit h-fit gap-1 bg-white my-6 rounded-md">
+          <input className="w-[221px] text-black m-2 px-2 text-md font-text" placeholder="Enter your email" />
+          <div className="flex-shrink-0">
+            <button className="flex items-center gap-2 m-1 py-1 px-4 bg-gradient-to-tr text-black font-semibold font-text from-[#017373] to-[#AAD9D1] rounded-lg">
+              Sign up
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="my-2"
+              >
+                <rect x="0.5" y="0.5" width="23" height="23" rx="4.5" stroke="#333333" />
+                <path d="M6.60583 6.43717V9.38062H12.2096V6.43717H6.60583Z" fill="#333333" />
+                <path
+                  d="M15.1535 6.43709L12.2099 9.38069L12.2088 9.38175L11.8276 9.38175L11.8276 9.76297L5.47052 16.1201L7.55132 18.2009L14.2907 11.4615L14.2923 17.0466L17.2353 17.046V9.38122L17.2337 8.51842L17.2343 8.51789L17.2343 6.43869L17.2327 6.43709L15.1535 6.43709Z"
+                  fill="#333333"
+                />
+              </svg>
+            </button>
           </div>
         </div>
-        <div className="my-16 mx-32 xs:my-4 xs:mx-8 flex-shrink-0 hidden xl:block">
-          <Image src={"/AbstractShapes.png"} width={363} height={483} alt="HeroSection" layout="intrinsic"></Image>
-        </div>
       </div>
+      <div className="hidden xl:block my-16 mx-32">
+        <Image src="/AbstractShapes.png" width={363} height={483} alt="HeroSection" />
+      </div>
+    </div>
 
       <div className="h-fit w-full justify-self-center overflow-clip">
         <Image src={"/Logos.png"} width={1440} height={200} alt="Logos" layout="responsive"/>
@@ -193,12 +213,12 @@ export default function Home() {
 
       <div className="flex flex-col w-full h-fit justify-center py-4 text-white">
         <div className="text-center px-4 sm:px-8 lg:px-16">
-          <h1 className="font-text font-bold text-4xl sm:text-5xl lg:text-6xl">
+          <h1 className="font-text font-bold text-4xl xs:text-2xl sm:text-5xl lg:text-6xl">
             <span className="bg-gradient-to-tr from-[#017373] to-[#AAD9D1] text-transparent bg-clip-text">
               Pricing
             </span> & Plans
           </h1>
-          <p className="font-text font-normal text-[#737373] px-4 sm:px-12 lg:px-24 mx-auto max-w-4xl text-wrap text-base sm:text-lg lg:text-xl">
+          <p className="font-text font-normal text-[#737373] px-4 sm:px-12 lg:px-24 mx-auto max-w-4xl text-wrap text-base sm:text-lg lg:text-xl xs:text-sm">
             With lots of unique blocks, you can easily build a page without coding. Build your next landing page.
           </p>
           <div className="flex justify-center gap-4 py-12 xs:py-8">
@@ -225,7 +245,7 @@ export default function Home() {
                 Individual Plan
               </p>
             </div>
-            <h1 className="font-text text-4xl sm:text-5xl lg:text-6xl text-black font-bold py-3 mx-5">
+            <h1 className="font-text text-4xl sm:text-5xl lg:text-6xl text-black font-bold py-3 mx-5 xs:text-3xl">
               $9.99<span className="text-lg sm:text-xl">/ month</span>
             </h1>
             {features1.map((feature, index) => (
@@ -282,7 +302,7 @@ export default function Home() {
                 Team Plan
               </p>
             </div>
-            <h1 className="font-text text-4xl sm:text-5xl lg:text-6xl text-white font-bold py-3 mx-5">
+            <h1 className="font-text text-4xl sm:text-5xl lg:text-6xl text-white font-bold py-3 mx-5 xs:text-3xl">
               $19.99<span className="text-lg sm:text-xl">/ month</span>
             </h1>
             {features2.map((feature, index) => (
@@ -340,7 +360,7 @@ export default function Home() {
                 Enterprise Plan
               </p>
             </div>
-            <h1 className="font-text text-4xl sm:text-5xl lg:text-6xl text-black font-bold py-3 mx-5">
+            <h1 className="font-text text-4xl sm:text-5xl lg:text-6xl text-black font-bold py-3 mx-5 xs:text-3xl">
               Custom
             </h1>
             {features3.map((feature, index) => (
